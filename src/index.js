6df3ac68c7dae378e5bdf51f257dd7e0bba41872
Juan Pablo Solana <juanpablosolana@gmail.com>
2021-09-11 12:57:32 -0500

    comentarios del puerto paralelo ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// La bascula modelo
+
+
 const express = require("express");
 const cors = require('cors')
 const app = express();
@@ -6,20 +9,23 @@ const Delimiter = require('@serialport/parser-delimiter');
 const port = new SerialPort('/dev/ttyUSB0', {
   baudRate: 2400
 })
+// El arreglo almacena los valores de la bascula
 let bascula = []
+// ------> Iniciando el server   <------
 app.use(cors())
 
+// Estoy utilizando delimiter para guardar los datos por renglon
 const parser = port.pipe(new Delimiter({ delimiter: '\n' }))
 parser.on('data', function (data) {
   // La conexión con el puerto siempre está abierta por lo que solo guardo los valores que cambian
   data.toString() !== bascula[bascula.length - 1]? bascula.push(data.toString()): null
 })
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
+// Al entrar a la url recupero el ultimo valor de la bascula
+app.get("/API/v1/bascula", (req, res) => {
   res.status(200).json({ bascula: bascula[bascula.length - 1] })
-  bascula=[]
+  //limpio el arreglo para mejorar el rendimiento
+  bascula.length=0
 })
 
 const PORT = process.env.PORT || 3000;
